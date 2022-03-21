af5e1c67079c88458e5a9496c71f13d837eddaf4
Beau Nouvelle <beau@getswifty.dev>
2022-03-21 19:09:42 +1100

    Initial Commit

---
+++ WebAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebAPI
+//
+//  Created by Beau Nouvelle on 21/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
